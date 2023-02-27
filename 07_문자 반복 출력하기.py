d35cf1abeba1cf351e6220bdf5fa04fc00c11978
jienyyyy <cyanred9@gmail.com>
2023-02-27 23:50:16 +0900

    220227 added

--- 07_문자 반복 출력하기.py
+++ 07_문자 반복 출력하기.py
@@ -3,3 +3,8 @@ def solution(my_string, n):
     for i in range(len(my_string)):
         result += my_string[i] * n 
     return result
+
+# 타인의 풀이 #
+
+def solution(my_string, n):
+    return ''.join(i*n for i in my_string) # join 안에서 한번에 for문으로 처리할 수 있었구나...
